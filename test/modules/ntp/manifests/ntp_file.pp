6cefd24e979072311e388d11e4f927b7f581f8c7
kamalkishorsingh <kamalkishorsingh3@gmail.com>
2017-11-10 06:49:13 +0530

    update ntp module

--- test/modules/ntp/manifests/ntp_file.pp
+++ test/modules/ntp/manifests/ntp_file.pp
@@ -1,14 +1,9 @@
 class ntp::ntp_file inherits ntp {
-$ntp_conf = '#manage by puppet
-server 192.168.0.29 iburst
-driftfile /var/lib/ntp/drift
-'
-
-file { '/etc/ntp.conf':
-     ensure => 'file',
-     content => $ntp_conf,
+ file { '/etc/ntp.conf':
+     ensure => 'present',
+     content => file('ntp/ntp_conf'),
      owner  => 'root',
      mode  => '0664',
-}
+  }
 }
 
--- test/modules/ntp/manifests/ntp_service.pp
+++ test/modules/ntp/manifests/ntp_service.pp
@@ -1,9 +1,21 @@
 class ntp::ntp_service inherits ntp {
-service { 'ntp':
+
+ case $facts['os']['family'] {
+	'RedHat' : {
+         $ntp_service = 'ntpd'
+         }
+	'Debian' : {
+         $ntp_service = 'ntp'
+         }
+         default : {
+         fail("your ${facts['os']['family']} is not tested")
+         }
+ }
+service { 'ntp_service':
       ensure => 'running',
       enable => true,
-      name  => 'ntp'
-}
+      name  =>  $ntp_service,
+        }
 
 }
 
